e3aa1b96cb9cea0b35b731c27a480bd11f69993a
Olivia Kim <kimhaejeong2525@gmail.com>
2022-05-25 16:29:43 -0400

    routes modified

---
+++ routes/apiRoutes.js
@@ -0,0 +1,16 @@
+const router= require('express').Router();
+const store = require('../db/store');
+
+router.get('/notes', (req,res)=>{
+    
+});
+
+router.post('/notes',(req,res)=>{
+
+});
+
+router.delete('/notes/:id',(req,res)=>{
+
+});
+
+module.exports= router; 
